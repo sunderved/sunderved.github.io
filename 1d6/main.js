a41d9bac25ad0252e5a78e11d3537308eb74a70b
sunderved <sunderved@users.noreply.github.com>
2016-06-19 15:41:54 -0700

    Add files via upload

--- 1d6/main.js
+++ 1d6/main.js
@@ -1,7 +1,76 @@
 function init()
 {
+	var vi = 17;     // interval between vertical dots
+	var md = vi*2    // margin between dots and borders
+	var ps = vi*8    // peep size
+	var ds = vi*30   // dice size
+	
+	var d1 = md;
+	var d2 = md+vi+ps;
+	var d3 = md+(vi+ps)*2;
+	
+	var el;
+	
+	el = document.getElementById('d1');
+	el.style.top = d1+'px';	
+	el.style.left = d1+'px';	
+	el.style.width = ps+'px';	
+	el.style.height = ps+'px';	
+	el.style.borderRadius = ps+'px';	
+
+	el = document.getElementById('d2');
+	el.style.top = d1+'px';	
+	el.style.left = d3+'px';	
+	el.style.width = ps+'px';	
+	el.style.height = ps+'px';	
+	el.style.borderRadius = ps+'px';	
+	
+	el = document.getElementById('d3');
+	el.style.top = d2+'px';	
+	el.style.left = d1+'px';	
+	el.style.width = ps+'px';	
+	el.style.height = ps+'px';	
+	el.style.borderRadius = ps+'px';	
+
+	el = document.getElementById('d4');
+	el.style.top = d2+'px';	
+	el.style.left = d3+'px';	
+	el.style.width = ps+'px';	
+	el.style.height = ps+'px';	
+	el.style.borderRadius = ps+'px';	
+		
+	el = document.getElementById('d5');
+	el.style.top = d3+'px';	
+	el.style.left = d1+'px';	
+	el.style.width = ps+'px';	
+	el.style.height = ps+'px';	
+	el.style.borderRadius = ps+'px';	
+
+	el = document.getElementById('d6');
+	el.style.top = d3+'px';	
+	el.style.left = d3+'px';	
+	el.style.width = ps+'px';	
+	el.style.height = ps+'px';	
+	el.style.borderRadius = ps+'px';	
+
+	el = document.getElementById('d7');
+	el.style.top = d2+'px';	
+	el.style.left = d2+'px';	
+	el.style.width = ps+'px';	
+	el.style.height = ps+'px';	
+	el.style.borderRadius = ps+'px';	
+
+	el = document.getElementById('dice');
+	el.style.width = ds+'px';	
+	el.style.height = ds+'px';	
+	el.style.borderRadius = (ds/10)+'px';	
+
+	el = document.getElementById('container');
+	el.style.width = ds+'px';	
+	el.style.height = ds+'px';	
+						
 	centerContainer();
-	showDots(1);
+	rollDice();
 }
 
 function roll()
@@ -12,7 +81,6 @@ function roll()
 function rollDice()
 {
 	var r = Math.floor(Math.random() * 6) + 1 ;
-	console.log('rolled '+r);
 	showDots(r);
 }
 
