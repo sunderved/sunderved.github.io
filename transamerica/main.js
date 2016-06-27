a2975598d0215bcac7addae11c3172baeb629656
sunderved <sunderved@users.noreply.github.com>
2016-06-27 10:01:26 -0700

    Add files via upload

--- transamerica/main.js
+++ transamerica/main.js
@@ -536,6 +536,17 @@ function debug(str)
 // 	console.log('   '+str);
 }
 
+// When the manifest file has changed and the browser has updated the files, 
+// it won’t use them for the current session. The application must be reloaded 
+window.applicationCache.addEventListener('updateready', function(e) {
+	if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+	  // A changed manifest file has been found and downloaded by
+	  // the browser. Swap cache and reload the page to use the new files.
+	  console.log('A changed manifest file has been found and downloaded. Reloading app');
+	  window.applicationCache.swapCache();
+	  window.location.reload();
+	}
+}, false);
 
 
 
